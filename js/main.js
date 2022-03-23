c2beaec80cd5a79075bfe5299b5dd3491f7ba9e4
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-03-24 01:35:11 +0500

    Added minus plus and bandge

--- js/main.js
+++ js/main.js
@@ -39,156 +39,116 @@ let sub = 0;
 let tax = 0;
 let total = 0;
 
-for (let i = 0; i < elArray.length; i++) {
+let newPizzaArray = [];
+
+elArray.forEach((el) => {
   let elLi = document.createElement("li");
   elLi.className = "main-item d-flex animate__animated animate__backInUp";
   elLi.innerHTML = `
       <div class="main-img-holder">
-        <img src="${elArray[i].imgUrl}" alt="${elArray[i].name}">
+        <img src="${el.imgUrl}" alt="${el.name}">
       </div>
       <div class="item-info">
         <h3 class="item-name">
-          ${elArray[i].name}
+          ${el.name}
         </h3>
         <span class="price">
-          $${elArray[i].price}
+          $${el.price}
         </span>
-        <button class="item-btn addBtn" onclick='addItem(${elArray[i].id})'>
+        <button class="item-btn addBtn" onclick='addItem(${el.id})'>
           Add to Cart
         </button>
       </div>
     `;
 
   elMainList.appendChild(elLi);
-}
+});
 
 // CREATING CART
 
-let newArray = [];
-
 function addItem(id) {
-  for (let i = 0; i < elArray.length; i++) {
-    if (id == elArray[i].id) {
-      newArray.push(elArray[i]);
-    }
-  }
+  newPizzaArray.push(elArray.filter((item) => item.id === id)[0]);
+  addCard(newPizzaArray);
+}
 
-  for (let i = 0; i < newArray.length; i++) {
-    if (i == newArray.length - 1) {
-      let elLi = document.createElement("li");
+function addCard(cardPizzaArray){
+  let arr = cardPizzaArray;
+  let topArr = [];
 
-      elLi.className = "main-item d-flex animate__animated animate__backInUp light-blue";
-      elLi.innerHTML = `
+  arr.forEach((item) => {
+    if(arr != "") topArr.push(arr[0]);
+    arr = arr.filter((el) => {
+      return arr[0].id != el.id;
+    });
+  });
+
+  let li = 0;
+  let listArr = [];
+  total = 0;
+  sub = 0;
+  tax = 0;
+
+  topArr.forEach((item) => {
+    let count = cardPizzaArray.filter((element) => {
+      return element.id == item.id;
+    });
+    li = `
+      <li class="main-item d-flex animate__animated animate__backInUp light-blue">
         <div class="main-img-holder">
-          <img src="${newArray[i].imgUrl}" alt="${newArray[i].name}">
+          <img src="${item.imgUrl}" alt="${item.name}">
         </div>
         <div class="item-info">
           <h3 class="item-name">
-            ${newArray[i].name}
+            ${item.name}
           </h3>
           <span class="price">
-            ${newArray[i].price}
+            $${item.price}
           </span>
-          <button class="item-btn addBtn" onclick="removeItem(${i})">
-            Remove from Cart
+          <span class="count">
+            ${count.length}
+          </span>
+          <button class="item-btn addBtn me-2" onclick="addItem(${item.id})">
+            +
+          </button>
+          <button class="item-btn addBtn" onclick="remove(${item.id})">
+            -
           </button>
         </div>
-      `;
-
-      sub += newArray[i].price;
-      elSub.textContent = sub.toFixed(2);
-
-      tax = sub * 10 / 100;
-      elTax.textContent = tax.toFixed(2);
-
-      total = (sub + tax);
-      elTotal.textContent = total.toFixed(2);
-
-      elCartList.appendChild(elLi);
-    }
-  }
-}
-
-function removeItem(index) {
-  let newArrayRemove = [];
-
-  for (let i = 0; i < newArray.length; i++) {
-    if (index != i) {
-      newArrayRemove.push(newArray[i]);
-    }
-  }
-
-  newArray = newArrayRemove;
-
-  elCartList.innerHTML = "";
-
-  sub = 0;
-  tax = 0;
-  total = 0;
-  
-  for (let i = 0; i < newArray.length; i++) {
-    let elLi = document.createElement("li");
-
-    elLi.className = "main-item d-flex light-blue";
-    elLi.innerHTML = `
-      <div class="main-img-holder">
-        <img src="${newArray[i].imgUrl}" alt="${newArray[i].name}">
-      </div>
-      <div class="item-info">
-        <h3 class="item-name">
-          ${newArray[i].name}
-        </h3>
-        <span class="price">
-          ${newArray[i].price}
-        </span>
-        <button class="item-btn addBtn" onclick="removeItem(${i})">
-          Remove from Cart
-        </button>
-      </div>
+      </li>
     `;
 
-    sub += newArray[i].price;
-    elSub.textContent = sub.toFixed(2);
+    listArr.push(li);
+    elCartList.innerHTML = listArr.join("");
+  });
 
+  cardPizzaArray.forEach((item) => {
+    sub += item.price;
     tax = sub * 10 / 100;
-    elTax.textContent = tax.toFixed(2);
-
     total = (sub + tax);
-    elTotal.textContent = total.toFixed(2);
+  });
 
-    elCartList.appendChild(elLi);
-  }
-
-  if(newArray.length == 0){
-    sub = 0;
-    tax = 0;
-    total = 0;
-
-    elSub.textContent = sub;
-    elTax.textContent = tax;
-    elTotal.textContent = total;
-  }
+  elSub.textContent = sub.toFixed(2);
+  elTax.textContent = tax.toFixed(2);
+  elTotal.textContent = total.toFixed(2);
 }
 
+function remove(elId) {
+  let count = 0;
+  let a = [];
 
-let elArr = [1, 2, 1, 3, 2, 3, 3, 5];
-let b = [];
-let count = elArr.length;
-
-for(let i = 0; i < count; i++){
-  let k = [];
-  let l = [];
-  for(let j = 0; j < elArr.length; j++){
-    if(elArr[0] == elArr[j]){
-      k.push(elArr[j]);
-    } else{
-      l.push(elArr[j]);
+  newPizzaArray.forEach((element) => {
+    if(element.id === elId && count === 0){
+      count++;
+    } else {
+      a.push(element);
     }
+  });
+
+  newPizzaArray = a;
+  if(newPizzaArray.length === 0){
+    elCartList.innerHTML = "";
+  } else{
+    addCard(newPizzaArray);
   }
-  elArr = l;
-  if(k != "") b.push(k);
 }
-
-console.log(b);
-
 
